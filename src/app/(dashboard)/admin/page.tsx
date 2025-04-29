10f715681288df8bbb13a12f61f78c3a8ec2fdcd
alikhan0616 <ak098boy@gmail.com>
2025-04-29 19:28:35 +0500

    Created Count Chart UI

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,16 +1,33 @@
+import AttendanceChart from "@/components/AttendanceChart"
+import CountChart from "@/components/CountChart"
 import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
   return (
     <div className='p-4 gap-4 flex flex-col md:flex-row'>
       {/* LEFT */}
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
       {/* USER CARDS */}
       <div className="flex gap-4 justify-between flex-wrap">
        <UserCard type="student" />
        <UserCard type="parent" />
        <UserCard type="teacher" />
        <UserCard type="staff" />
+      </div>
+      {/* MIDDLE CHARTS */}
+      <div className="flex gap-4 flex-col lg:flex-row">
+        {/* COUNT CHART */}
+        <div className="w-full lg:w-1/3 h-[450px]">
+        <CountChart />
+        </div>
+        {/* ATTENDENCE CHART */}
+        <div className="w-full lg:w-2/3 h-[450px]">
+        <AttendanceChart />
+        </div>
+      </div>
+      {/* BOTTOM CHART */}
+      <div className="">
+
       </div>
       </div>
       {/* RIGHT */}
